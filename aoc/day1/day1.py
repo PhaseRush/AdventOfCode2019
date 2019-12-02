46758cd4a4fa9b0ff085443647fb353b81437112
phaserush <leozhang888@yahoo.ca>
2019-12-01 21:50:43 -0800

    Day 2, fixed naming for day 1

--- aoc/day1/day1.py
+++ aoc/day1/day1.py
@@ -1,20 +1,20 @@
 accumulator = 0
 cache = dict([(-2, 0), (-1, 0), (0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0)])
 
-def determinecost(mass):
+
+def determine_cost(mass):
     # if already calculated this
     if mass in cache:
         return cache[mass]
     # new input
-    fuelmass = mass // 3 - 2
-    finalmass = fuelmass + determinecost(fuelmass)
-    cache[mass] = finalmass
-    return finalmass
+    fuel_mass = mass // 3 - 2
+    final_mass = fuel_mass + determine_cost(fuel_mass)
+    cache[mass] = final_mass
+    return final_mass
 
 
 with open('day1/input1.txt', 'r') as file:
     for line in file:
-        accumulator += determinecost(int(line))
+        accumulator += determine_cost(int(line))
 
 print(accumulator)
-print('dicts'.replace('t', 'k'))
---
+++ aoc/day2/day2.py
@@ -0,0 +1,30 @@
+import copy
+
+start_state = list(map(int, open('input.txt', 'r').readline().split(',')))
+
+
+def try_codes(noun, verb):
+    opcodes = copy.deepcopy(start_state)
+    opcodes[1] = noun
+    opcodes[2] = verb
+
+    curr_idx = 0
+    while curr_idx < len(opcodes) and opcodes[curr_idx] != 99:
+        this_op = opcodes[curr_idx]
+        first = opcodes[opcodes[curr_idx + 1]]
+        second = opcodes[opcodes[curr_idx + 2]]
+        third = opcodes[curr_idx + 3]
+        if this_op is 1:
+            opcodes[third] = first + second
+        elif this_op is 2:
+            opcodes[third] = first * second
+
+        curr_idx += 4
+
+    return opcodes[0]
+
+
+for noun in range(100):
+    for verb in range(100):
+        if try_codes(noun, verb) == 19690720:
+            print(noun * 100 + verb)
