d7d78c0b3b4a060755881663680011dbc7c21707
phaserush <leozhang888@yahoo.ca>
2019-12-07 22:11:39 -0800

    cleaned up day 8, set up day 9

--- aoc/day8/day8.py
+++ aoc/day8/day8.py
@@ -1,3 +1,5 @@
+from collections import Counter as nyaaaa
+
 string = str(open('input.txt', 'r').read())
 
 wide = 25
@@ -7,24 +9,15 @@ block_size = wide * tall
 
 blocks = [string[i:i + block_size] for i in range(0, len(string), block_size)]
 
-block_num = -1
+ans = None
 min_zeros = 50000000000
-for i in range(len(blocks)):
-    block = blocks[i]
-    this_zeros = 0
-    for char in block:
-        if char == '0':
-            this_zeros += 1
-    if this_zeros < min_zeros:
-        min_zeros = this_zeros
-        block_num = i
-num_1 = 0
-num_2 = 0
-for char in blocks[block_num]:
-    if char == '1':
-        num_1 += 1
-    elif char == '2':
-        num_2 += 1
+for block in blocks:
+    counter = nyaaaa(block)
+    if counter['0'] < min_zeros:
+        min_zeros = counter['0']
+        ans = counter['1'] * counter['2']
+
+print(ans)
 
 blocks = [line.strip() for line in list(open('input.txt', 'r').readline())]
 
@@ -40,16 +33,8 @@ def gen_pixel(x, y):
             layer += 1
 
 
-final_pix = []
 for y in range(tall):  # 6
     for x in range(wide):  # 25
-        final_pix.append(gen_pixel(x, y))
-
-counter = 0
-for num in final_pix:
-    if counter % wide == 0:
-        print()
-    else:
-        print(num if num == '1' else '.', end='')
-
-    counter += 1
+        curr_pix = gen_pixel(x, y)
+        print(curr_pix if curr_pix == '1' else ' ', end='')
+    print()
