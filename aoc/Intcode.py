fbb82059dc567f5e36f88cd41a1249fb5b3b8586
phaserush <leozhang888@yahoo.ca>
2019-12-10 22:17:44 -0800

    day 11, stole someones Intcode.py

---
+++ aoc/Intcode.py
@@ -0,0 +1,80 @@
+from collections import defaultdict
+
+
+class Program(object):
+    def __init__(self, pid, program_file, input):
+        self.P = defaultdict(int)
+        for i, x in enumerate(open(program_file).read().split(',')):
+            self.P[i] = int(x)
+        self.input = input
+        self.ip = 0
+        self.pid = pid
+        self.rel_base = 0
+
+    def idx(self, i, I):
+        mode = (0 if i >= len(I) else I[i])
+        val = self.P[self.ip + 1 + i]
+        if mode == 0:
+            pass  # no-op
+        elif mode == 2:
+            val = val + self.rel_base
+        else:
+            assert False, mode
+        return val
+
+    def val(self, i, I):
+        mode = (0 if i >= len(I) else I[i])
+        val = self.P[self.ip + 1 + i]
+        if mode == 0:
+            val = self.P[val]
+        elif mode == 2:
+            val = self.P[val + self.rel_base]
+        return val
+
+    def run_all(self):
+        ans = []
+        while True:
+            val = self.run()
+            if val == None:
+                return ans
+            ans.append(val)
+
+    def run(self):
+        """Return next output"""
+        while True:
+            cmd = str(self.P[self.ip])
+            opcode = int(cmd[-2:])
+            I = list(reversed([int(x) for x in cmd[:-2]]))
+            if opcode == 1:
+                i1, i2 = self.val(0, I), self.val(1, I)
+                self.P[self.idx(2, I)] = self.val(0, I) + self.val(1, I)
+                self.ip += 4
+            elif opcode == 2:
+                i1, i2 = self.val(0, I), self.val(1, I)
+                self.P[self.idx(2, I)] = self.val(0, I) * self.val(1, I)
+                self.ip += 4
+            elif opcode == 3:
+                inp = self.input()
+                self.P[self.idx(0, I)] = inp  # self.Q[0]
+                # self.Q.pop(0)
+                self.ip += 2
+            elif opcode == 4:
+                ans = self.val(0, I)
+                self.ip += 2
+                return ans
+            elif opcode == 5:
+                self.ip = self.val(1, I) if self.val(0, I) != 0 else self.ip + 3
+            elif opcode == 6:
+                self.ip = self.val(1, I) if self.val(0, I) == 0 else self.ip + 3
+            elif opcode == 7:
+                self.P[self.idx(2, I)] = (1 if self.val(0, I) < self.val(1, I) else 0)
+                self.ip += 4
+            elif opcode == 8:
+                self.P[self.idx(2, I)] = (1 if self.val(0, I) == self.val(1, I) else 0)
+                self.ip += 4
+            elif opcode == 9:
+                self.rel_base += self.val(0, I)
+                self.ip += 2
+            else:
+                assert opcode == 99, opcode
+                return None
---
+++ aoc/day11/day11.py
@@ -0,0 +1,36 @@
+from aoc.Intcode import Program
+
+R = 30
+C = 1000
+map = [[0 for _ in range(C)] for _ in range(R)]
+map[R // 2][C // 2] = 1
+curr_dir = 0
+DIR = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+pos = (R // 2, C // 2)
+
+
+def get_color():
+    return map[pos[0]][pos[1]]
+
+
+painted = set()
+P = Program('0', 'input.txt', get_color)
+while True:
+    colour = P.run()
+    turn = P.run()
+    if colour is None or turn is None:
+        break
+
+    painted.add(pos)
+    map[pos[0]][pos[1]] = colour
+    if turn == 0:
+        curr_dir = (curr_dir + 3) % 4
+    else:
+        curr_dir = (curr_dir + 1) % 4
+    pos = (pos[0] + DIR[curr_dir][0], pos[1] + DIR[curr_dir][1])
+
+print(f'painted={len(painted)}')
+for r in range(R):
+    for c in range(C):
+        print('X' if map[r][c] == 1 else ' ', end='')
+    print()
