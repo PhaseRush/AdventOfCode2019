4b21fa631f59cac0ead8dadc4561b72b8370cc0d
phaserush <leozhang888@yahoo.ca>
2019-12-12 22:24:25 -0800

    day 13 clean up

--- aoc/day13/Day13.py
+++ aoc/day13/Day13.py
@@ -2,13 +2,11 @@
 from collections import defaultdict
 from aoc.Intcode import Program
 
-Grid = defaultdict(int)
+grid = defaultdict(int)
 
 
 def determine_input():
-    # xs = sorted([x for (x, _) in Grid])
-    # ys = sorted([y for (_, y) in Grid])
-    for (k, v) in Grid.items():
+    for (k, v) in grid.items():
         if v == 3:
             paddle = k
         elif v == 4:
@@ -21,15 +19,16 @@ def determine_input():
         return 0
 
 
-P = Program('0', 'input.txt', determine_input)
-P.P[0] = 2
+Intcode = Program('0', 'input.txt', determine_input)
+Intcode.P[0] = 2
 score = 0
-while not P.halted:
-    x = P.run()
-    y = P.run()
-    c = P.run()
+while not Intcode.halted:
+    x = Intcode.run()
+    y = Intcode.run()
+    c = Intcode.run()
     if x == -1 and y == 0:
         score = c
     else:
-        Grid[(y, x)] = c
+        grid[(y, x)] = c
+
 print(score)
